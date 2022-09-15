19d1b07b0063553d8ef43ef8127b4866c0d3800c
soup1997 <soup1997@naver.com>
2022-09-15 19:32:19 +0900

    2022-09-15

--- Arduino/Motor_Controller/Motor_Controller.ino
+++ Arduino/Motor_Controller/Motor_Controller.ino
@@ -15,14 +15,10 @@
  *              11 (PWM)
  */
 /**************************/
-
-// include library
 #include <ros.h>
 #include <Servo.h>
 #include <ackermann_msgs/AckermannDriveStamped.h>
 
-
-//////////////////////////////////////////////////
 
 // PIN Connection
 const int servo_motor = 10;
@@ -30,20 +26,20 @@ const int motor_1 = 6;
 const int motor_2 = 7;
 const int motor_pwm = 11;
 
-// Set maximum and minimum limits for the PWM values
+// Controlling Pwm speed, Angle Value
+unsigned int motor_speed = 0;
+unsigned int motor_angle = 0;
+
+// Set maximum and minimum limits for the Angle values
 const int ANGLE_MIN = 40;
 const int ANGLE_MAX = 140;
 
-// Set maximum and minimum limits for the Motor Speed values
+// Set maximum and minimum limits for the PWM values
 const int SPEED_MIN = 150;
 const int SPEED_MAX = 250;
 
 // Create Servo Motor Instance
 Servo servo;
-
-// Controlling Speed, Angle PWM Value
-int motor_speed = 0;
-int motor_angle = 0;
 
 // Function Declaration
 void ackermannCallback(const ackermann_msgs::AckermannDriveStamped & ackermann);
@@ -51,6 +47,7 @@ void motor_forward(void);
 void motor_backward(void);
 void motor_stop(void);
 void calc_pwm_values(int speed, int angle);
+void motor_init(void);
 void motor_control(int speed, int angle);
 
 // ROS Init
@@ -58,9 +55,7 @@ ros::Subscriber<ackermann_msgs::AckermannDriveStamped> ackermannSubscriber("/ack
 ros::NodeHandle nh;
 
 
-//////////////////////////////////////////////////
-
-// Callback func.
+// Callback function
 void ackermannCallback(const ackermann_msgs::AckermannDriveStamped & ackermann)
 {
   int sub_speed = ackermann.drive.speed;
@@ -86,6 +81,7 @@ void motor_stop(void){
 // 
 void calc_pwm_values(int speed, int angle)
 {
+  // Controlling Speed, Angle PWM Value
   int pwm_speed = 0, pwm_angle = 0;
   
   // Motor direction setting
@@ -137,15 +133,12 @@ void calc_pwm_values(int speed, int angle)
 // Motor Control
 void motor_control(int speed, int angle){
   calc_pwm_values(speed, angle);
-  
   servo.write(motor_angle);
   analogWrite(motor_pwm, motor_speed);
 }
 
-
-//////////////////////////////////////////////////
-
-void setup(){
+void motor_init()
+{
   servo.attach(servo_motor);
   servo.write(90);
   
@@ -156,32 +149,19 @@ void setup(){
   digitalWrite(motor_1, HIGH);
   digitalWrite(motor_2, LOW);
   
-  motor_control(0, 0);    // speed(-50~50), angle(-50~50)
+  analogWrite(motor_pwm, 0);
 }
-
-
 //////////////////////////////////////////////////
 
+void setup(){
+  motor_init();
+  
+  // ROS Setup
+  nh.getHardware() -> setBaud(115200);
+  nh.initNode();
+  nh.subscribe(ackermannSubscriber);
+}
+
 void loop(){
-  /*
-  // forward fast, left
-  motor_control(30, -50);    // speed(-50~50), angle(-50~50)
-  delay(3000);
-
-  // forward slow, middle
-  motor_control(5, 0);    // speed(-50~50), angle(-50~50)
-  delay(3000);
-
-  // backward slow, right
-  motor_control(-15, 23);    // speed(-50~50), angle(-50~50)
-  delay(3000);
-  
-  // backward fast, left
-  motor_control(-50, -10);    // speed(-50~50), angle(-50~50)
-  delay(3000);
-  
-  // stop, middle
-  motor_control(0, 0);    // speed(-50~50), angle(-50~50)
-  delay(3000);
-  */
+  nh.spinOnce(); // call subscriber callback function
 }
