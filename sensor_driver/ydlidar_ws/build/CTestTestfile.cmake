67b70ac2adf7516cbb716ac98bd32358ae88fbeb
soup1997 <soup1997@naver.com>
2022-09-18 23:54:35 +0900

    Delete sensor_driver directory

--- sensor_driver/ydlidar_ws/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/aeslab/embe_ws/src/ydlidar_ws/src
-# Build directory: /home/aeslab/embe_ws/src/ydlidar_ws/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("ydlidar_ros_driver")
